18e21d2471db49f53e8f68eadcad56e195929702
PhrogLover <dan.h.champion@durham.ac.uk>
2023-04-28 00:29:08 +0100

    setup eslint on backend

--- server/app.js
+++ server/app.js
@@ -8,105 +8,105 @@ app.use(express.json());
 // CORS Policy
 const cors = require('cors');
 const corsOptions = {
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+	origin:'http://localhost:3000',
+	credentials:true, //access-control-allow-credentials:true
+	optionSuccessStatus:200
 };
 app.use(cors(corsOptions));
 
 const users = [
-    {
-        "id": 1,
-        "name": "Dan",
-        "icon": ""
-    },
-    {
-        "id": 2,
-        "name": "Panos",
-        "icon": ""
-    },
-    {
-        "id": 3,
-        "name": "Dai",
-        "icon": ""
-    }
+	{
+		'id': 1,
+		'name': 'Dan',
+		'icon': ''
+	},
+	{
+		'id': 2,
+		'name': 'Panos',
+		'icon': ''
+	},
+	{
+		'id': 3,
+		'name': 'Dai',
+		'icon': ''
+	}
 ];
 
 const continue_watching_list = [
-    {
-        "id": 1,
-        "title": "Breaking Bad",
-        "timestamp": null
-    },
-    {
-        "id": 2,
-        "title": "Power Rangers: SPD",
-        "timestamp": null
-    },
-    {
-        "id": 3,
-        "title": "Scott Pilgrim vs The World",
-        "timestamp": null
-    }
+	{
+		'id': 1,
+		'title': 'Breaking Bad',
+		'timestamp': null
+	},
+	{
+		'id': 2,
+		'title': 'Power Rangers: SPD',
+		'timestamp': null
+	},
+	{
+		'id': 3,
+		'title': 'Scott Pilgrim vs The World',
+		'timestamp': null
+	}
 ];
 
 const watchlist = [
-    {
-        "id": 4,
-        "title": "Flapjack"
-    },
-    {
-        "id": 5,
-        "title": "Daredevil"
-    },
-    {
-        "id": 6,
-        "title": "Scott Pilgrim vs The World"
-    }
+	{
+		'id': 4,
+		'title': 'Flapjack'
+	},
+	{
+		'id': 5,
+		'title': 'Daredevil'
+	},
+	{
+		'id': 6,
+		'title': 'Scott Pilgrim vs The World'
+	}
 ];
 
 const random_movies = [
-    {
-        "id": 7,
-        "title": "Ant-Man"
-    },
-    {
-        "id": 8,
-        "title": "Spirited Away"
-    },
-    {
-        "id": 9,
-        "title": "Drive"
-    }
+	{
+		'id': 7,
+		'title': 'Ant-Man'
+	},
+	{
+		'id': 8,
+		'title': 'Spirited Away'
+	},
+	{
+		'id': 9,
+		'title': 'Drive'
+	}
 ];
 
 app.get('/users', (req, res) => {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.json(users);
+	res.status(200);
+	res.setHeader('Content-Type', 'application/json');
+	res.json(users);
 });
 
 app.get('/contwatch', (req, res) => { // Might need to change name of endpoint
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.json(continue_watching_list);
+	res.status(200);
+	res.setHeader('Content-Type', 'application/json');
+	res.json(continue_watching_list);
 });
 
 app.get('/watchlist', (req, res) => { // Might need to change name of endpoint
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.json(watchlist);
+	res.status(200);
+	res.setHeader('Content-Type', 'application/json');
+	res.json(watchlist);
 });
 
 app.get('/movies', (req, res) => { // Might need to change name of endpoint
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.json(random_movies);
+	res.status(200);
+	res.setHeader('Content-Type', 'application/json');
+	res.json(random_movies);
 });
 
 app.listen(
-    PORT,
-    () => console.log(`Running on http://localhost:${PORT}`)
+	PORT,
+	() => console.log(`Running on http://localhost:${PORT}`)
 );
 
 module.exports = app;
--- server/app.test.js
+++ server/app.test.js
@@ -5,82 +5,82 @@ const app = require('./app');
 
 describe('Test all GET methods in app.js', () => {
 
-    // Testing GET Methods
-    // Users Section
-    test('GET /users successful', () => {
-        return request(app)
-        .get('/users')
-        .expect(200);
-    });
-    test('GET /users returns JSON', () => {
-        return request(app)
-        .get('/users')
-        .expect('Content-type', /json/);
-    });
-    test('GET /users returns JSON containing Dan', () => {
-        return request(app)
-        .get('/users')
-        .expect(/Dan/);
-    });
-    test('GET /users returns JSON with 3 characters', async () => {
-        let response = await request(app).get('/users');
-        let usersLength = JSON.parse(JSON.stringify(response.body)).length;
-        expect(usersLength).toBe(3);
-    });
+	// Testing GET Methods
+	// Users Section
+	test('GET /users successful', () => {
+		return request(app)
+			.get('/users')
+			.expect(200);
+	});
+	test('GET /users returns JSON', () => {
+		return request(app)
+			.get('/users')
+			.expect('Content-type', /json/);
+	});
+	test('GET /users returns JSON containing Dan', () => {
+		return request(app)
+			.get('/users')
+			.expect(/Dan/);
+	});
+	test('GET /users returns JSON with 3 characters', async () => {
+		let response = await request(app).get('/users');
+		let usersLength = JSON.parse(JSON.stringify(response.body)).length;
+		expect(usersLength).toBe(3);
+	});
 
-    // Continue Watching Section
-    test('GET /contwatch successful', () => {
-        return request(app)
-        .get('/contwatch')
-        .expect(200);
-    });
-    test('GET /contwatch returns JSON', () => {
-        return request(app)
-        .get('/contwatch')
-        .expect('Content-type', /json/);
-    });
-    test('GET /contwatch returns JSON containing Breaking Bad', () => {
-        return request(app)
-        .get('/contwatch')
-        .expect(/Breaking Bad/);
-    });
-    test('GET /contwatch returns JSON with 3 characters', async () => {
-        let response = await request(app).get('/contwatch');
-        let usersLength = JSON.parse(JSON.stringify(response.body)).length;
-        expect(usersLength).toBe(3);
-    });
+	// Continue Watching Section
+	test('GET /contwatch successful', () => {
+		return request(app)
+			.get('/contwatch')
+			.expect(200);
+	});
+	test('GET /contwatch returns JSON', () => {
+		return request(app)
+			.get('/contwatch')
+			.expect('Content-type', /json/);
+	});
+	test('GET /contwatch returns JSON containing Breaking Bad', () => {
+		return request(app)
+			.get('/contwatch')
+			.expect(/Breaking Bad/);
+	});
+	test('GET /contwatch returns JSON with 3 characters', async () => {
+		let response = await request(app).get('/contwatch');
+		let usersLength = JSON.parse(JSON.stringify(response.body)).length;
+		expect(usersLength).toBe(3);
+	});
 
-    // Watchlist Section
-    test('GET /watchlist successful', () => {
-        return request(app)
-        .get('/watchlist')
-        .expect(200);
-    });
-    test('GET /watchlist returns JSON', () => {
-        return request(app)
-        .get('/watchlist')
-        .expect('Content-type', /json/);
-    });
-    test('GET /watchlist returns JSON containing Daredevil', () => {
-        return request(app)
-        .get('/watchlist')
-        .expect(/Daredevil/);
-    });
-    test('GET /watchlist returns JSON with 3 characters', async () => {
-        let response = await request(app).get('/watchlist');
-        let usersLength = JSON.parse(JSON.stringify(response.body)).length;
-        expect(usersLength).toBe(3);
-    });
+	// Watchlist Section
+	test('GET /watchlist successful', () => {
+		return request(app)
+			.get('/watchlist')
+			.expect(200);
+	});
+	test('GET /watchlist returns JSON', () => {
+		return request(app)
+			.get('/watchlist')
+			.expect('Content-type', /json/);
+	});
+	test('GET /watchlist returns JSON containing Daredevil', () => {
+		return request(app)
+			.get('/watchlist')
+			.expect(/Daredevil/);
+	});
+	test('GET /watchlist returns JSON with 3 characters', async () => {
+		let response = await request(app).get('/watchlist');
+		let usersLength = JSON.parse(JSON.stringify(response.body)).length;
+		expect(usersLength).toBe(3);
+	});
 
-    // Random Movies Section
-    test('GET /movies successful', () => {
-        return request(app)
-        .get('/movies')
-        .expect(200);
-    });
-    test('GET /movies returns JSON', () => {
-        return request(app)
-        .get('/movies')
-        .expect('Content-type', /json/);
-    });
+	// Random Movies Section
+	test('GET /movies successful', () => {
+		return request(app)
+			.get('/movies')
+			.expect(200);
+	});
+	test('GET /movies returns JSON', () => {
+		return request(app)
+			.get('/movies')
+			.expect('Content-type', /json/);
+	});
 });
