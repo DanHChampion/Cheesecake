3ae4db4c7676a1bc175713dea6efeafa1b153d56
PhrogLover <dan.h.champion@durham.ac.uk>
2023-04-27 22:27:09 +0100

    created basic not found page, if URL is invalid

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home.js';
 import Users from './components/users/Users.js';
+import NotFound from './components/notfound/NotFound.js';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 				<Routes>
           <Route path="/users" element={<Users/>} />
 					<Route path="/home" element={<Home/>} />
+          <Route path="/*" element={<NotFound/>} />
 				</Routes>
 			</BrowserRouter>
     </div>
---
+++ client/src/components/notfound/NotFound.js
@@ -0,0 +1,13 @@
+import './NotFound.css';
+import NavBar from '../basic/NavBar.js'
+
+const NotFound = () => {
+    return(
+        <div className="NotFound">
+            <NavBar/>
+            <h2>Page Not Found</h2>
+        </div>
+    )
+}
+
+export default NotFound;
