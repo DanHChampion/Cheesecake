b8c1f42b20947a11e90256b9c7b9b8e88a4ec98f
PhrogLover <dan.h.champion@durham.ac.uk>
2023-04-30 22:04:24 +0100

    refactor carousel

--- client/src/components/basic/Carousel.js
+++ client/src/components/basic/Carousel.js
@@ -31,15 +31,15 @@ const Carousel = (props) => {
 	useEffect(() => getItems(), []);
 
 	const goLeft = () => {
-		if (scrollX >= 1000) {
-			wrapperRef.current.scrollTo(scrollX - 1000, 0);
+		if (scrollX >= wrapperRef.current.offsetWidth) {
+			wrapperRef.current.scrollTo(scrollX - wrapperRef.current.offsetWidth, 0);
 		} else {
 			wrapperRef.current.scrollTo(0, 0);
 		}
 	};
 	const goRight = () => {
-		if (scrollX - 1000 <= containerRef.current.offsetWidth) {
-			wrapperRef.current.scrollTo(scrollX + 1000, 0);
+		if (scrollX - wrapperRef.current.offsetWidth <= containerRef.current.offsetWidth) {
+			wrapperRef.current.scrollTo(scrollX + wrapperRef.current.offsetWidth, 0);
 		} else {
 			wrapperRef.current.scrollTo(containerRef.current.offsetWidth, 0);
 		}
@@ -50,7 +50,7 @@ const Carousel = (props) => {
 		setScrollX(wrapperRef.current.scrollLeft);
 		setState({
 			left:  scrollX > 1 ? true : false,
-			right: scrollX <= containerRef.current.offsetWidth - 1200 ? true : false
+			right: scrollX <= containerRef.current.offsetWidth - wrapperRef.current.offsetWidth ? true : false
 		});
 		console.log(scrollX);
 	};
