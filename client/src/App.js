5b52d07243660571cbee9e965d69f5cb90337c7a
PhrogLover <dan.h.champion@durham.ac.uk>
2023-05-01 20:28:30 +0100

    folder restructure and fix lint

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home.js';
 import Users from './components/users/Users.js';
--- client/src/components/basic/_tests/NavBar.test.js
+++ client/src/components/basic/_tests/NavBar.test.js
@@ -1,25 +1,24 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 // import { createRoot } from 'react-dom/client';
-import NavBar from './NavBar.js';
+import NavBar from '../NavBar';
 
 it('renders all items NavBar.js successfully', () => {
 	render(<NavBar/>);
-    const logoImgElement = screen.getAllByRole('img')[0];
+	const logoImgElement = screen.getAllByRole('img')[0];
 	expect(logoImgElement).toBeInTheDocument();
 	const homeElement = screen.getByText(/Home/i);
 	expect(homeElement).toBeInTheDocument();
-    const watchlistElement = screen.getByText(/Watchlist/i);
+	const watchlistElement = screen.getByText(/Watchlist/i);
 	expect(watchlistElement).toBeInTheDocument();
 	const moviesElement = screen.getByText(/Movies/i);
 	expect(moviesElement).toBeInTheDocument();
 	const seriesElement = screen.getByText(/Series/i);
 	expect(seriesElement).toBeInTheDocument();
-    const searchBarElement = screen.getByRole('textbox');
+	const searchBarElement = screen.getByRole('textbox');
 	expect(searchBarElement).toBeInTheDocument();
-    const searchElement = screen.getByRole('button');
+	const searchElement = screen.getByRole('button');
 	expect(searchElement).toBeInTheDocument();
-    const profileImgElement = screen.getAllByRole('img')[1];
-    expect(profileImgElement).toBeInTheDocument();
+	const profileImgElement = screen.getAllByRole('img')[1];
+	expect(profileImgElement).toBeInTheDocument();
 });
